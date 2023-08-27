d5a3d2c27955c907d77ccafd9472880263b38ebb
tobiashelfenstein <login@tobiashelfenstein.de>
2023-08-27 11:31:24 +0200

    Grundstruktur angelegt

---
+++ myarbor/baumkataster/doctype/feld/feld.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Tobias Helfenstein and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Feld", {
+// 	refresh(frm) {
+
+// 	},
+// });
