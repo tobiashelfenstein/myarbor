0d30cc92bad101c9ce9e626d1ea6f42d9694125e
tobiashelfenstein <login@tobiashelfenstein.de>
2024-06-02 13:47:24 +0200

    DocType Baumtyp hinzugefügt und Änderungen am DocType Kategorie

--- myarbor/tree_register/doctype/rest_tree/rest_tree.js
+++ myarbor/tree_register/doctype/rest_tree/rest_tree.js
@@ -1,12 +1,37 @@
 // Copyright (c) 2023, Tobias Helfenstein and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Rest Tree", {
-	on_submit: function(frm) {
-		frappe.msgprint({
-			title: __('Notification'),
-			indicator: 'green',
-			message: __('Ruheplätze werden automatisch generiert.')
-			});
-	},
+//frappe.ui.form.on("Rest Tree", {
+//	on_submit: function(frm) {
+//		frappe.msgprint({
+//			title: __('Notification'),
+//			indicator: 'green',
+//			message: __('Ruheplätze werden automatisch generiert.')
+//			});
+//	},
+//});
+
+frappe.provide("myarbor.tree_register");
+myarbor.tree_register.RestTreeController = class RestTreeController extends frappe.ui.form.Controller {
+	diameter() {
+		frappe.db.get_doc('Category', null, { min_diameter: '30' })
+			.then(doc => {
+				frm.set_value("category", doc.name);
+				console.log(doc)
+			})
+	}
+};
+
+cur_frm.cscript = new myarbor.tree_register.RestTreeController({
+	frm: cur_frm
+});
+
+frappe.ui.form.on("Rest Tree", "refresh", function(frm) {
+	frm.set_df_property("tree_type", "read_only", frm.doc.gravesites_count > 0 ? true : false);
+	//if (frm.doc.gravesites_count > 0) {
+	//	frm.toggle_enable('tree_type', false);
+	//}
+	//else {
+	//	frm.toggle_enable('tree_type', true);
+	//}
 });
--- myarbor/tree_register/doctype/rest_tree/rest_tree.py
+++ myarbor/tree_register/doctype/rest_tree/rest_tree.py
@@ -2,26 +2,46 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
 import re
+import pyproj
+import json
 
 class RestTree(Document):
+
+    # autoname document
+    def autoname(self):
+        # define document name
+        self.tree_name = _("Rest Tree {0}").format(self.tree_number)
+
     # this method will run every time a document is saved
     def before_save(self):
-        self.tree_name = f'Rest Tree {self.tree_number}'
 
+        # update gravesite count
         self.gravesites_count = 0
         for key, value in self.__dict__.items():
             if re.match(r"gravesite\d+", key) and value:
-                # update gravesite count
                 self.gravesites_count += value
 
-                # auto generate gravesite
-                #gravesite = frappe.get_doc({"doctype":"Gravesite", "rest_tree":self.tree_number, "gravesite_number":re.sub(r"gravesite", "", key)})
-                #gravesite.insert(ignore_permissions=True)
+        # update GeoJSON
+        if self.x_value and self.y_value:
+            proj = pyproj.Transformer.from_crs(25832, 4326, always_xy=True)
+            x, y = proj.transform(self.x_value, self.y_value)
+
+            self.location = json.dumps({
+                    "type":"FeatureCollection",
+                    "features":[
+                        {
+                            "type":"Feature",
+                            "properties":{},
+                            "geometry":{"type":"Point","coordinates":[x,y]},
+                        }
+                        ],
+                    })
 
     def on_update(self):
-        # update gravesite on submit
+        # update gravesite if changed
         for key, value in self.__dict__.items():
             if re.match(r"gravesite\d+", key):
                 gravesite_number = re.sub(r"gravesite", "", key)
@@ -36,4 +56,3 @@ class RestTree(Document):
                     if frappe.db.exists({"doctype": "Gravesite", "rest_tree":self.tree_number, "gravesite_number":gravesite_number}):
                         gravesite = frappe.get_last_doc('Gravesite', filters={"rest_tree":self.tree_number, "gravesite_number":gravesite_number})
                         gravesite.delete()
-
